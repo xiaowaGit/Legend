9072278464738ea74b8a6726300c93749210b939
tony <598291606@qq.com>
2019-01-20 20:02:42 +0800

    utf-8 player
    utf-8 player

--- game-server/app/servers/scene/base/Player.ts
+++ game-server/app/servers/scene/base/Player.ts
@@ -3,7 +3,7 @@ import { MainScene, PTerm } from "../drive/MainScene";
 import { PActor } from "./PActor";
 
 /*
-     ???
+     角色类
 */
 export class Player extends Actor {
     
@@ -11,12 +11,12 @@ export class Player extends Actor {
         super(name,map_w,map_h,scene,pactor);
     }
 
-    ////????????????
+    ////向地图的所有玩家推送消息
     public notice_all_player(onType:string,body:Object):void {
         this._scene.notice_all_player(onType,body);
     }
 
-    /// ?????????
+    /// 向某个玩家通知消息
     public notice_one_player(onType:string,body:Object):void {
         this._scene.notice_one_player(onType,body,this.name);
     }
